41cb288faf3f939d67dde8eee8cf9cbd94ffead6
Yurii Holiuk <bionicuss@gmail.com>
2019-08-29 14:57:56 +0300

    add task

---
+++ src/smashWords.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Implement smashWords function:
+ *
+ * Function takes an array of words
+ * and smashes them together into a sentence and returns the sentence.
+ * You can ignore any need to sanitize words or add punctuation,
+ * but you should add spaces between each word.
+ * Be careful,
+ * there shouldn't be a space at the beginning or the end of the sentence!
+ * Should return empty string if array is empty.
+ *
+ * const words = ['hello', 'world', 'this', 'is', 'great'];
+ * smashWords(words) ===  'hello world this is great';
+ *
+ * @param {string[]} words
+ *
+ * @return {string}
+ */
+function smashWords(words) {
+  // write code here
+}
+
+module.exports = smashWords;
---
+++ src/smashWords.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const smashWords = require('./smashWords');
+
+function getRandomInt(min, max) {
+  return Math.floor(
+    Math.random() * (Math.ceil(max) - Math.floor(min) + 1)
+  ) + min;
+}
+
+const createSentence = length => {
+  let count = 0;
+  let sentence = '';
+
+  while (count < length) {
+    if (Math.random() < 0.15) {
+      sentence += ' ';
+      count++;
+    } else {
+      sentence += String.fromCharCode(getRandomInt(97, 122));
+    }
+  }
+
+  return sentence;
+};
+
+describe('smashWords', () => {
+  it('Should return a string', () => {
+    const result = smashWords(['a', 'b']);
+    expect(result).toBe(String(result));
+  });
+
+  it('Should return correct sentence', () => {
+    expect(smashWords(['I', 'love', 'JS!'])).toBe('I love JS!');
+  });
+
+  it('Should return empty string if array is empty', () => {
+    expect(smashWords([])).toBe('');
+  });
+
+  it('Should correct smash random words', () => {
+    for (let i = 0; i <= 10; i++) {
+      const sentence = createSentence(i);
+
+      expect(smashWords(sentence.split(' '))).toBe(sentence);
+    }
+  });
+});
